fada1134da32a0eddd28670126c4ddddf63df9e0
WalterMitty2112 <devonhope20@gmail.com>
2020-04-13 01:36:16 -0400

    fixed major bugs

--- lib_app/app.py
+++ lib_app/app.py
@@ -1,58 +1,90 @@
 import func as func
 import current_user as cu
 
-#ACTS AS MENU
-def signin():
+def signin(user):
     loop = 0
     while(loop == 0):
         uname = input("Username: ")
         pswd = input("Password: ")
         res = func.load_user(uname, pswd)
-        print(res)
-        print(res["user_pswd"][0])
-        if(res["user_pswd"][0] == pswd and res["user_username"][0] == uname):
-            print("You're signed in!")
-            print()
-            sin_menu()
-            loop=1
+        #print(res['user_pswd'])
+        #print(res['user_id'])
+        if(res['user_id'] or res is None):
+            #print(res['user_pswd'][0])
+            if(res['user_pswd'][0] == pswd and res['user_username'][0] == uname):
+                user.newUser(res['user_id'][0], res['user_name'][0], res['user_username'][0],\
+                res['user_biladdr'][0],res['user_shipaddr'][0],{'id':res['user_id'][0], 'books':{}})
+                print("You're signed in!")
+                print()
+                print("Signed in as: " + user.name)
+                print()
+                return 1
+                loop=1
         else:
             ans = input("Thats not right, try again?(y or n) ")
             if(ans == 'n' or ans == 'N'):
                 loop = 1
+                return 0
 
 #ACTS AS MENU
-def browse_menu():
+def browse_menu(user):
     e = 0
     while(e == 0):
-        ops = ["Sign in to buy book", "More info on specific book", "Main menu"]
-        choice = func.pr_menu(ops)
-        if(choice == 0):
-            signin()
-        elif(choice == 1):
-            print(func.mor_book())
-        elif(choice == 2):
-            e = 1
+        opso = ["Sign in to buy book", "More info on specific book", "Main menu"] #signed out
+        opsi = ["Add to cart", "More info on specific book", "Main menu"] #signed in
+        if(user.getID() is None):
+            choice = func.pr_menu(opso)
+            if(choice == 0):
+                signin(user)
+            if(choice == 1):
+                func.mor_book()
+            elif(choice == 2):
+                e = 1
+            else:
+                print("Thats not an option")
+                print()
         else:
-            print("Thats not an option")
+            choice = func.pr_menu(opsi)
+            if(choice == 0):
+                user = func.addCart(user)
+            if(choice == 1):
+                func.mor_book()
+            elif(choice == 2):
+                e = 1
+            else:
+                print("Thats not an option")
+                print()
+
+def check_menu(user):
+    e = 0
+    while(not e):
+        ops = [""]
+        #print cart, prices and total
 
 #ACTS AS MENU
-def sin_menu():
+def sin_menu(user):
     #New signed in user menu
     exit = 0
     while(exit == 0):
-        ops=["Browse","Search","Cart","Sign out", "Exit"]
+        ops=["Browse","Search","Cart","Checkout","Sign out", "Exit"]
         print("Please select an option from the list below by number")
         choice = func.pr_menu(ops)
         if(choice == 0):
-            print(func.browse())
+            func.browse()
+            browse_menu(user)
         elif(choice == 1):
             func.search()
         elif(choice == 2):
-            func.cart()
+            #displays users cart
+            func.cart(user)
         elif(choice == 3):
+            func.update_cart(user)
+            check_menu(user)
+        elif(choice == 4):
             exit = 1
             print("Signed out!")
-        elif(choice == 4):
+            main()
+        elif(choice == 5):
             exit = 1
             print("Come back soon!")
         else:
@@ -60,23 +92,29 @@ def sin_menu():
 
 def main():
     print("Welcome to Look Inna Book Bookstore!")
-    signin = False
-    while(signin == False):
-        ops= ["Sign in", "Sign up", "Browse books", "Exit"]
+    sign = False
+    while(not sign):
+        ops= ["Sign in", "Sign up", "Search", "Browse books", "Exit"]
         print("Please select an option from the list below by number")
         choice = func.pr_menu(ops)
         if(choice == 0):
-            signin()
-            signin=True
+            if(signin(user)):
+                sin_menu(user)
+                sign=True
         elif(choice == 1):
             func.signup()
         elif(choice == 2):
-            print(func.browse())
-            browse_menu()
+            func.search()
         elif(choice == 3):
-            signin = True
+            func.browse()
+            browse_menu(user)
+            if(user.getID()):
+                sin_menu(user)
+        elif(choice == 4):
+            sign = True
             print("Come back soon!")
         else:
             print("Thats not an option")
 
+user = cu.cuser(None, None, None, None, None, None)
 main()
--- lib_app/func.py
+++ lib_app/func.py
@@ -6,6 +6,8 @@ import cred_pgsql as cred
 import pandas.io.sql as psgql
 from random import randint
 import current_user as cu
+import pprint
+import string
 
 # Set up a connection to the postgres server.
 conn_string = "host="+ cred.PGHOST +" port="+ "5432" +" dbname="+ cred.PGDATABASE +" user=" + cred.PGUSER \
@@ -17,15 +19,36 @@ print("Connected!")
 cursor = conn.cursor()
 schema = "allops"
 
+def insert_db(sql_command):
+    try:
+        cursor.execute(sql_command, conn)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def load_db(sql_command):
+    res = None
+    try:
+        data = pd.read_sql(sql_command, conn)
+        res = data.to_dict()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return res
+
 def load_user(uname, pswd):
     table = "user"
     uname = "'"+uname+"'"
     pswd = "'"+pswd+"'"
     sql_command = "select * from {}.{} where user_username = {} and user_pswd = {};".format(str(schema), str(table), uname, pswd)
-    data = pd.read_sql(sql_command, conn)
-    #print(data)
-    data = data.to_dict()
+    data = load_db(sql_command)
     return data
+
+def update_cart(user):
+    table="book"
+    con = "update {}.{} set ch_userid = {}, ch_billaddr = {}, ch_shipaddr = {}, ch_books = {};".format(str(schema),str(table),str(user.getID()), str(user.getBA()), str(user.getSA()), str(user.getCheck()))
+    insert_db(con)
+    print("Cart added to DB")
 
 def num_row(table):
     row_count = "select count(*) from {}.{};".format(str(schema),str(table))
@@ -40,39 +63,195 @@ def add_user(name,uname,email,pswd):
     pswd = "'"+pswd+"'"
     id = num_row(table)
     sql_command = "insert into {}.{} (user_id, user_name, user_username, user_email, user_pswd) values ({},{},{},{},{});".format(str(schema), str(table),str(id),str(name),str(uname),str(email),str(pswd))
-    cursor.execute(sql_command, conn)
+    insert_db(sql_command)
     print("You've been added!")
 
 def pr_menu(ops):
+    res = None
     for index, op in enumerate(ops):
         print("("+str(index)+") " +op)
-    return int(input("- "))
+    try:
+        res = int(input("- "))
+    except ValueError:
+        print("Thats not an option")
+        print()
+    except KeyboardInterrupt:
+        print("Come again!")
+        sys.exit()
+    return res
 
 
 def signup():
-    print("Please enter the following information to create your Look Inna Book account!")
-    name = input("Name: ")
-    uname = input("Username: ")
-    email = input("Email: ")
-    pswd = input("Password: ")
-    add_user(name, uname, email, pswd)
+    loop = 0
+    while(not loop):
+        print("Enter 'e' to exit")
+        print("Please enter the following information to create your Look Inna Book account!")
+        name = input("Name: ")
+        if(name == 'e'):
+            loop = 1
+        uname = input("Username: ")
+        if(uname == 'e' ):
+            loop = 1
+        email = input("Email: ")
+        if(email == 'e'):
+            loop = 1
+        pswd = input("Password: ")
+        if(pswd == 'e'):
+            loop = 1
+        else:
+            add_user(name, uname, email, pswd)
+            loop=1
 
 def browse():
     sql_command = "SELECT * FROM {}.{};".format(str(schema), "book")
-    #print (sql_command)
     # Load the data
-    data = pd.read_sql(sql_command, conn)
-    data = data.to_dict()
-    return data
+    data = load_db(sql_command)
+    if(not data):
+        print("Sorry! No books to be found here!")
+        print("Try another day!")
+        print()
+    print("Books on hand:")
+    d = {"Name" : "Price"}
+    p = {}
+    pprint.pprint(d)
+    for i in data["bk_name"]:
+        pr = "$" + str(data["bk_price"][i])
+        p.update({data["bk_name"][i] : pr})
 
+    pprint.pprint(p)
+
+def addCart(user):
+    loop = 0
+    while( not loop):
+        print("Enter 'e' to exit")
+        book = input("Enter the name of the book: ")
+        if(book == 'e'):
+            loop = 1
+        book = string.capwords(str(book))
+        book = "'"+book+"'"
+        #print(book)
+        if(book):
+            sql_command = "SELECT * FROM {}.{} where bk_name = {};".format(str(schema), "book", str(book))
+            res = load_db(sql_command)
+            if not res["bk_name"]:
+                print("That is not a book")
+                ans = input("Do you want to add another book?(y or n) ")
+                if(ans == 'n' or ans == 'N'):
+                    loop = 1
+                    print()
+            else:
+                #print(res)
+                user.setCheckout(res)
+                loop = 1
+
+    return user
+
+def cart(user):
+    cart_books = user.getCheck()
+    #print(cart_books)
+    print()
+    print("CART:")
+    for b in cart_books:
+        print(b)
+    print()
 
 def mor_book():
-    name = input("Name of book: ")
-    name = "'"+name+"'"
-    sql_command = "SELECT * FROM {}.{} where bk_name = {};".format(str(schema), "book", str(name))
-    data = pd.read_sql(sql_command, conn)
-    #print(data)
-    return data
+    loop = 0
+    while(not loop):
+        print("Enter 'e' to exit")
+        name = input("Name of book: ")
+        if(name == 'e'):
+            loop = 1
+        name = string.capwords(str(name))
+        name = "'"+name+"'"
+        sql_command = "SELECT * FROM {}.{} where bk_name = {};".format(str(schema), "book", str(name))
+        data = load_db(sql_command)
+        if(not data["bk_id"]):
+            print("Sorry that book doesnt exist!")
+            ans = input("Try another?(y or n) ")
+            if(ans == 'n' or ans == 'N'):
+                loop == 1
+        else:
+            d = []
+            p = {"Name": None}, {"Price" : None}, {"Author" : None},{"ISBN-13": None},{"Genre":None},{"Pages":None},{"Type":None},{"Language":None},{"Publisher": None},{"Published" : None}
+            for i in data["bk_name"]:
+                pr = "$" + str(data["bk_price"][i])
+                p = {"Name": data["bk_name"][i]}, {"Price" : pr}, {"Author" : data["bk_auth"][i]},{"ISBN-13": data["bk_isbn"][i]},{"Genre":data["bk_genre"][i]},{"Pages":data["bk_pages"][i]},{"Type":data["bk_type"][i]},{"Language":data["bk_lang"][i]},{"Publisher": data["bk_pub"][i]},{"Published" : data["bk_published"][i]}
+                d.append(p)
+            print()
+            print("More info:")
+            pprint.pprint(d)
+            print()
+            loop = 1
+
+def search():
+    loop = 0
+    while(not loop):
+        print()
+        print("Enter 'e' to exit")
+        print("You can enter a book name, author, isbn, price, etc.")
+        print("To enter more than one attribute, separate them by a ',' ")
+        print()
+        se = input("Search: ")
+        allq = []
+        if(se):
+            if(se == 'e'):
+                loop = 1
+            #loop through se to find and separate by commas
+            elif(',' in se):
+                #se is now a list
+                se = se.split(',')
+                for q in se:
+                    q = string.capwords(q)
+                    q = "'"+q+"'"
+                    com = "select * from global_search({})".format(q)
+                    res = load_db(com)
+                    allq.append(res)
+            else:
+                se = string.capwords(se)
+                se = "'"+se+"'"
+                com = "select * from global_search({})".format(se)
+                res = load_db(com)
+                allq.append(res)
+
+            emptys = 0
+            for r in allq:
+                #print(r)
+                if(r['schemaname']):
+                    if(r['schemaname'][0]):
+                        #find the book and print it
+                        allbooks = []
+                        #if one query results in multiples
+                        if(len(r['schemaname']) > 1):
+                            #print(r['rowctid'][0])
+                            for i in range(len(r['rowctid'])):
+                                s = "'"+r['rowctid'][i]+"'"
+                                #print(s)
+                                exactq = "select * from {}.{} where ctid = {}; ".format(str(r['schemaname'][0]), str(r['tablename'][0]),str(s))
+                                res = load_db(exactq)
+                                p={}
+                                for i in res["bk_name"]:
+                                    pr = "$" + str(res["bk_price"][i])
+                                    p.update({res["bk_name"][i] : pr})
+                                pprint.pprint(p)
+                        else:
+                            s = "'"+r['rowctid'][0]+"'"
+                            exactq = "select * from {}.{} where ctid = {};".format(str(r['schemaname'][0]), str(r['tablename'][0]),str(s))
+                            res = load_db(exactq)
+                            p={}
+                            for i in res["bk_name"]:
+                                pr = "$" + str(res["bk_price"][i])
+                                p.update({res["bk_name"][i] : pr})
+                            pprint.pprint(p)
+                else:
+                    emptys += 1
+                    if(emptys == len(allq)):
+                        ans = input("Didn't find anyting, wanna try again? ")
+                        if(ans == 'n' or ans == 'N'):
+                            loop = 1
+            loop = 1
+        else:
+            print("Please enter anything or 'e' to exit")
 
 """
 def load_data(schema, table):
